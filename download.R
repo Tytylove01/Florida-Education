c1f38f0e65349861145d0d56bcd3ca8350fac9d3
Tytylove01 <tyaveonlcute@gmail.com>
2025-01-22 10:48:54 -0500

    data stuff

---
+++ download.R
@@ -0,0 +1,15 @@
+library(tidycensus)
+library(tidyverse)
+library(leaflet)
+library(sf)
+library(viridis)
+
+
+fl_education_data <- get_acs(
+  geography = "tract",  # You can also use "tract" or "block group" for more granularity
+  variables = "B15003_022",  # Less than 9th grade education
+  state = "FL",  # Florida
+  year = 2021,  # Specify the year of the ACS data
+  survey = "acs5",  # 5-year ACS estimates
+  geometry = TRUE  # Set to TRUE if you want spatial data
+)
